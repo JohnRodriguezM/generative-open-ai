29acae249044692b0d0883f3a333b2de0123c0f1
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2023-01-03 19:52:49 -0500

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import "dotenv/config";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h3>hello</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+declare var process: {
+  env: {
+    [key: string]: string | undefined;
+  };
+};
+
+
+import "dotenv/config";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
