afe8978bd9395bdb844ec978ab35ee491aa6ff03
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2023-01-14 22:13:36 -0500

    test deleted

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default{
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/*.test.tsx'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-};
